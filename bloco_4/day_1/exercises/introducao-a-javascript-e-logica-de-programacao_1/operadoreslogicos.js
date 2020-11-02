07e10c720988acca00ad9757d687dfd7c4694092
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-02 18:09:56 -0300

    add logical op

---
+++ bloco_4/day_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/operadoreslogicos.js
@@ -0,0 +1,32 @@
+//adiction
+let a = 10
+let b = 2
+let sum = a + b
+console.log(sum);
+
+//subtraction
+let a = 10
+let b = 2
+let sub = a - b
+console.log(sub);
+
+//multiplication
+let a = 10
+let b = 2
+let mult = a * b
+console.log(mult);
+
+//division
+let a = 10
+let b = 2
+let div = a / b
+console.log(div);
+
+//module
+let a = 10
+let b = 2
+let mod = a ** b
+console.log(mod)
+
+
+
