a50f84d0fe318d00678e6ee6076080278529ffff
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-02 18:30:35 -0300

    add 2.returning the bigger of three numbers.

--- bloco_4/day_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/moreexercises.js
+++ bloco_4/day_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/moreexercises.js
@@ -1,9 +1,20 @@
-//1.returning the greater of two numbers.
+//1.returning the bigger of two numbers.
 let a = 0;
 let b = 2;
 if (a > b) {
     console.log(a)
 } else {
     console.log(b)
+}
 
-//2.
+//2.returning the bigger of three numbers.
+let a = 0;
+let b = 2;
+let c = -5;
+if (a > b && a > c) {
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
