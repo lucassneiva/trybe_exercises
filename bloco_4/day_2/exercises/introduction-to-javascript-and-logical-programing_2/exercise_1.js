f2b1d666ebe42a901c8e92735dfa9b50ce4c95d1
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-03 13:40:50 -0300

    add 5 loop for

--- bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
+++ bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
@@ -35,3 +35,12 @@ if (media > 20) {
 }
 
 //5.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0
+for (let index = 0; index < numbers.length; index++) {
+    for (let cont = 0; cont < index;) {
+         (numbers[index] > numbers[cont]) {
+                console.log(numbers[index])
+            }
+        }
+}
