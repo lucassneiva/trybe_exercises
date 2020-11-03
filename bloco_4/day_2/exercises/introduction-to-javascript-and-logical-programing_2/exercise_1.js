14e97e09b6a341c009008a6d047bb570536b8193
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-03 10:20:21 -0300

    add exercise 4 loop for

--- bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
+++ bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
@@ -20,3 +20,18 @@ for (let index = 0; index < numbers.length; index++) {
     media = resultado / 10
 }
 console.log(media);
+
+//4.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0
+for (let index = 0; index < numbers.length; index++) {
+    resultado += numbers[index]
+    media = resultado / 10
+}
+if (media > 20) {
+    console.log("Valor maior que 20")
+} else {
+    console.log("Valor menor ou igual a 20")
+}
+
+//5.
