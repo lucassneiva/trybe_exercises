71ea17c77d363fc0bd1680b3a9a7ab694b79c3db
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-03 10:14:14 -0300

    add exercise 3 loop for

--- bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
+++ bloco_4/day_2/exercises/introduction-to-javascript-and-logical-programing_2/exercise_1.js
@@ -11,3 +11,12 @@ for (let index = 0; index < numbers.length; index++) {
     resultado += numbers[index]
 }
 console.log(resultado);
+
+//3.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0
+for (let index = 0; index < numbers.length; index++) {
+    resultado += numbers[index]
+    media = resultado / 10
+}
+console.log(media);
