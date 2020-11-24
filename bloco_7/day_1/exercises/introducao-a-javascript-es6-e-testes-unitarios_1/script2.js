729a6d0003c158990c8727ca4857f70c80a272e7
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-23 21:30:28 -0300

    add oddsandevens.sort

---
+++ bloco_7/day_1/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script2.js
@@ -0,0 +1,3 @@
+arrayOddsAndEvens = [13, 3, 4, 10, 7, 2].sort((a, b) => a - b);
+const oddsAndEvens = () => `Os números ${arrayOddsAndEvens} se encontram ordenados de forma crescente!`
+console.log(oddsAndEvens(arrayOddsAndEvens));
